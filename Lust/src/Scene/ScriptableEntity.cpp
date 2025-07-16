5bedd1c4d1c9c7f95a7d232c142ea3a58db50b82
bmarques1995 <testebash@outlook.com>
2025-07-16 00:30:30 -0300

    removed defective function

--- Lust/src/Scene/ScriptableEntity.cpp
+++ Lust/src/Scene/ScriptableEntity.cpp
@@ -1,15 +1,3 @@
 #include "ScriptableEntity.hpp"
-#include "Components.hpp"
 
-void LustInternal_DestroyEntity(Lust::Entity* entity)
-{
-	while (entity->HasComponent<Lust::NativeScriptComponent>())
-	{
-		entity->GetComponent<Lust::NativeScriptComponent>()->Instance->OnDestroy();
-		entity->RemoveComponent<Lust::NativeScriptComponent>();
-	}
-	entity->RemoveAllComponents<Lust::TilemapComponent>();
-	entity->RemoveAllComponents<Lust::CameraComponent>();
-	entity->RemoveAllComponents<Lust::TransformComponent>();
-	entity->RemoveAllComponents<Lust::SpriteRendererComponent>();
-}
+
